5d307d648643d273114b7ccff244f6d5d0702095
lily-coder <lilyondie@gmail.com>
2021-11-30 16:27:12 +0300

    Fix linter errors

--- student.rb
+++ student.rb
@@ -1,13 +1,12 @@
 require './person'
 
 class Student < Person
-    def initialize(age, name = "Unknown", parent_permission = true, classroom)  
-      super(age, name, parent_permission)
-      @classroom = classroom
-    end
+  def initialize(age, name = 'Unknown', parent_permission = true, classroom)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-      "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
-  
