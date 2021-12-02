e31bf270a188a0cc2d78cfc9ddc63f4cf24489cb
lily-coder <lilyondie@gmail.com>
2021-12-02 12:02:39 +0300

    Add class Rental with setters and getters for dates

--- student.rb
+++ student.rb
@@ -1,5 +1,7 @@
 require './person'
 
+attr_reader :classroom
+
 class Student < Person
   def initialize(age, name = 'Unknown', parent_permission = true, classroom)
     super(age, name, parent_permission)
@@ -8,5 +10,9 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
