4543e3fc128941e0d1069619a53cb9a444b10870
lily-coder <lilyondie@gmail.com>
2021-12-04 21:52:22 +0300

    Fix minor problems

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age, :rental
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @corrector = Corrector.new
     @id = Random.rand(1..1000)
     @name = name
